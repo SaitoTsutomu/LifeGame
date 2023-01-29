20c76214faafcfd4d03b5d6fb3f4daa4b147ba12
Saito Tsutomu <tsutomu7@hotmail.co.jp>
2023-01-29 13:45:33 +0900

    upd

--- core.py
+++ core.py
@@ -17,21 +17,14 @@ def add_geometry(obj: bpy.types.Object) -> None:
     modifier.node_group = node_group
     node_group.inputs.new("NodeSocketGeometry", "Geometry")
     node_group.outputs.new("NodeSocketGeometry", "Geometry")
-    ndgi = node_group.nodes.new("NodeGroupInput")
-    ndgo = node_group.nodes.new("NodeGroupOutput")
-    ndip = node_group.nodes.new("GeometryNodeInstanceOnPoints")
-    ndpo = node_group.nodes.new("GeometryNodeInputPosition")
-    ndsx = node_group.nodes.new("ShaderNodeSeparateXYZ")
-    ndcm = node_group.nodes.new("FunctionNodeCompare")
-    ndcb = node_group.nodes.new("GeometryNodeMeshCube")
-    node_group.links.new(ndgi.outputs[0], ndip.inputs[0])
-    node_group.links.new(ndip.outputs[0], ndgo.inputs[0])
-    node_group.links.new(ndpo.outputs[0], ndsx.inputs[0])
-    node_group.links.new(ndsx.outputs[2], ndcm.inputs[0])
-    node_group.links.new(ndcm.outputs[0], ndip.inputs[1])
-    node_group.links.new(ndcb.outputs[0], ndip.inputs[2])
-    node_group.links.new(ndip.outputs[0], ndgo.inputs[0])
-    nds = [ndgi, ndgo, ndip, ndpo, ndsx, ndcm, ndcb]
+    ss = (
+        "NodeGroupInput NodeGroupOutput GeometryNodeInstanceOnPoints GeometryNodeInputPosition "
+        "ShaderNodeSeparateXYZ FunctionNodeCompare GeometryNodeMeshCube"
+    ).split()
+    nds = [node_group.nodes.new(s) for s in ss]
+    lks = [[0, 2], [2, 1], [3, 4], [24, 5], [5, 12], [6, 22], [2, 1]]
+    for i, j in lks:
+        node_group.links.new(nds[i % 10].outputs[i // 10], nds[j % 10].inputs[j // 10])
     poss = [[80, 40], [410, 40], [250, 40], [-240, -99], [-80, -99], [80, -99], [-400, -28]]
     for nd, pos in zip(nds, poss):
         nd.location = pos
@@ -45,29 +38,29 @@ def make_grid(context, nx, ny):
     bpy.ops.object.mode_set(mode="EDIT")
 
 
-def make_anim(obj, nx, ny, ncycle, unit):
+def make_anim(context, obj, nx, ny, ncycle, unit):
     bpy.ops.object.mode_set(mode="OBJECT")
+    obj.data.animation_data_clear()
     vtx = np.array(obj.data.vertices).reshape(ny, nx)
     cells = np.zeros((ny + 2, nx + 2), bool)
     for y, x in product(range(ny), range(nx)):
         cells[y + 1, x + 1] = vtx[y, x].select
     ss = [slice(None, -2), slice(1, -1), slice(2, None)]  # 前中後用のスライス
     sc = ss[1]  # 中央用のスライス
-    bpy.context.scene.frame_end = ncycle * unit + 1
+    context.scene.frame_end = ncycle * unit + 1
     for tm in range(ncycle + 1):
-        bpy.context.scene.frame_current = tm * unit + 1
+        context.scene.frame_current = tm * unit + 1
         for y, x in product(range(ny), range(nx)):
-            vtx[y, x].co.z = cells[y + 1, x + 1] * 0.5
-        for v in vtx.flat:
-            v.keyframe_insert("co")
-        if unit > 4:
-            bpy.context.scene.frame_current += 3
-            for v in vtx.flat:
-                v.keyframe_insert("co")
-        new = np.sum([cells[s1, s2] for s1 in ss for s2 in ss if s1 != sc or s2 != sc], 0)
-        n2, n3 = new == 2, new == 3
+            pre, nxt = vtx[y, x].co.z, cells[y + 1, x + 1] * 0.5
+            if tm == 0 or (nxt != pre):
+                if tm and unit > 1:
+                    vtx[y, x].keyframe_insert("co", frame=context.scene.frame_current - 1, index=2)
+                vtx[y, x].co.z = nxt
+                vtx[y, x].keyframe_insert("co", index=2)
+        cum = np.sum([cells[s1, s2] for s1 in ss for s2 in ss if s1 != sc or s2 != sc], 0)
+        n2, n3 = cum == 2, cum == 3
         cells[sc, sc] = cells[sc, sc] & n2 | n3
-    bpy.context.scene.frame_current = 1
+    context.scene.frame_current = 1
     bpy.ops.screen.animation_play()
 
 
@@ -79,8 +72,9 @@ class CLG_OT_make_sample(bpy.types.Operator):
     bl_description = "Make sample."
 
     def execute(self, context):
-        context.scene.nx, context.scene.ny, context.scene.ncycle = 15, 15, 8
-        make_grid(context, context.scene.nx, context.scene.ny)
+        sc = context.scene
+        sc.nx, sc.ny, sc.ncycle = 15, 15, 8
+        make_grid(context, sc.nx, sc.ny)
         obj = bpy.context.edit_object
         bm = bmesh.from_edit_mesh(obj.data)
         bm.verts.ensure_lookup_table()
@@ -90,9 +84,7 @@ class CLG_OT_make_sample(bpy.types.Operator):
             bm.verts[15 * x + y + 93].select = bm.verts[15 * x + y + 55].select = True
         # bm.free()
         bpy.ops.object.mode_set(mode="OBJECT")
-        make_anim(
-            obj, context.scene.nx, context.scene.ny, context.scene.ncycle, context.scene.unit
-        )
+        make_anim(context, obj, sc.nx, sc.ny, sc.ncycle, sc.unit)
         return {"FINISHED"}
 
 
@@ -127,7 +119,7 @@ class CLG_OT_make_anim(bpy.types.Operator):
         if not obj or obj.type != "MESH" or nx * ny != len(obj.data.vertices):
             self.report({"WARNING"}, "Select a object.")
             return {"CANCELLED"}
-        make_anim(obj, nx, ny, self.ncycle, self.unit)
+        make_anim(context, obj, nx, ny, self.ncycle, self.unit)
         return {"FINISHED"}
 
 
